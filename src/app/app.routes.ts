e8ecf3400ca6b926d3f5bbf8b7e1972e6b7b100a
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-06-28 15:20:12 -0300

    feat: add component agent e mapeia routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'agents' },
+  {
+    path: 'agents',
+    loadChildren: () => import('./agents/agents.module').then(m => m.AgentsModule)
+  }
+];
