82257833afe4770bc5be4299513874dec3c53c86
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-06-28 17:09:46 -0300

    feat: add componente table module

--- src/app/agents/agents/agents.component.ts
+++ src/app/agents/agents/agents.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { Agent } from '../model/agent';
 
 @Component({
   selector: 'app-agents',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './agents.component.html',
   styleUrl: './agents.component.scss'
 })
 export class AgentsComponent {
 
+  agents: Agent[] = [
+    { _id: '1', name: 'Agent 1', type: 'Tipo 1' }
+  ];
+  displayedColumns = ['name', 'type'];
+
+  constructor() {
+    // this.agents = [];
+  }
 }
---
+++ src/app/agents/model/agent.ts
@@ -0,0 +1,5 @@
+export interface Agent {
+  _id: string;
+  name: string;
+  type: string;
+}
